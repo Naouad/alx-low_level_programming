4d368c6c14e11162b2fcd1d008bdb0821bac5ab3
Naouad <naima.aouad@hotmail.com>
2023-03-16 18:54:00 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
-   *  main - Entry
-    *  Return: Always 0
-     */
+*  main - Entry
+*  Return: Always 0
+*/
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
