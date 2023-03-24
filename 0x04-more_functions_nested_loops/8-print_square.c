2877bf818bacd6d7cabfe7f55405e85d66ce4733
Naouad <naima.aouad@hotmail.com>
2023-03-23 19:15:02 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - prints a square of size
+ * @size: input size of the square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int k;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		for (k = 0; k < (size); k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
