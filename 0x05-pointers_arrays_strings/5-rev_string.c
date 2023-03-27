c3582e884de8fe20c716aff2a9d14c1595c54b23
Naouad <naima.aouad@hotmail.com>
2023-03-27 14:13:39 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: the given string to be reversed
+ * Return: nothing
+ */
+
+void	rev_string(char *s)
+{
+	int len;
+	int i;
+	int d;
+	char tmp;
+
+	len = 0;
+	i = 0;
+
+	while (s[len] != '\0')
+		len++;
+	
+	d = len / 2;
+
+	while (d--)
+	{
+		tmp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = tmp;
+		i++;
+	}
+}
