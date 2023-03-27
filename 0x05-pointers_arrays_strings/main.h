5dabcd4001c859164f1557434194e057cc3b7fca
Naouad <naima.aouad@hotmail.com>
2023-03-27 14:34:15 -0700

    task 8

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 #include <unistd.h>
 
 void reset_to_98(int *n);
@@ -11,5 +12,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 
 #endif
