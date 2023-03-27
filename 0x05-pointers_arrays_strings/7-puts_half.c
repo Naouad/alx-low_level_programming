6df3d89198eaee4631f7e08f24a3e2032244ccbd
Naouad <naima.aouad@hotmail.com>
2023-03-27 14:25:32 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: the given string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
