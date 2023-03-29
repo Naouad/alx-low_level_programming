9c35adb66bc19d9578cda97a1a8c9ff2b25ce818
Naouad <naima.aouad@hotmail.com>
2023-03-29 16:15:20 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - lowercase to uppercase
+ * @s: input string
+ * Return: string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
