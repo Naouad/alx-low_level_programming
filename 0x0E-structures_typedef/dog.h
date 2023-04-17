db336ca4affbf625430c0ac29930aecb328afdd7
Naouad <naima.aouad@hotmail.com>
2023-04-17 16:55:12 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Define a new type struct dog
+ * with the elements: nam, age, owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
