78a5036f9a9002b06d26cd5f7299763e288f55d5
Naouad <naima.aouad@hotmail.com>
2023-04-17 17:32:19 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - dog structure
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
  * Description: Define a new type struct dog
  * with the elements: nam, age, owner
  */
@@ -18,9 +19,14 @@ struct dog
 };
 
 /**
- * prototypes for the next tasks
+ * init_dog - initializes a struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ * Description: initializes the struct dog pointed to by d with th
+ * specified name, age, and owner
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
